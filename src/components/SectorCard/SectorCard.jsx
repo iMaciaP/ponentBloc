fcf866821aeff7f042c34bf1e774b14d0c1cc40b
InvelonCreator <imacia@invelon.com>
2022-08-30 17:01:08 +0200

    netlify

--- src/components/SectorCard/SectorCard.jsx
+++ src/components/SectorCard/SectorCard.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const SectorCard = (props) => {
   const { facils, med, dif, xtrem } = props;
 
-  const navigate =  useNavigate();
+  // const navigate =  useNavigate();
 
   const handleClick = (link) => {
     // navigate(link);
