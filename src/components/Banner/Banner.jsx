fbbf75dac8c127db43b8bde00a08320eec04ccc7
InvelonCreator <imacia@invelon.com>
2022-10-07 16:13:47 +0200

    landpage and footer

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Banner = () => {
   return (
     <Paper
       sx={{
         p: 2,
-        margin: "auto",
-        marginTop: "32px",
+        margin: "16px",
         flexGrow: 1,
-        /* backgroundColor: (theme) =>
-          theme.palette.mode === "dark" ? "#1A2027" : "#fff",*/
-        backgroundColor: "#F5C841",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "dark" ? "#1A2027" : "#1769aa",
+        // backgroundColor: "#F5C841",
       }}
     >
-      <Typography gutterBottom variant="h3" component="div">
-        Ponent Bloc
-      </Typography>
-      <Typography gutterBottom variant="h4" component="div">
-        Pagina en construcció
-      </Typography>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Bloc de ponent
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          paragraph
+        >
+          L'idea es tenir un repositori on poder consultar les vies i blocs que
+          hi ha per la zona de ponent de Catalunya.El projecte es públic a
+          github per a qui vulgui col·laborar.
+        </Typography>
+      </Container>
     </Paper>
   );
 };
--- src/components/Landpage/Landpage.jsx
+++ src/components/Landpage/Landpage.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import "./Landpage.css";
 import SectorCard from "../SectorCard/SectorCard";
 import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
 import { cardsInfo } from "../../data/data";
 
 const Landpage = () => {
   return (
-    <div className="container">
+    <>
       <Banner />
-      <div className="SectorCards">
-        {cardsInfo.map((element, index) => {
-          return <SectorCard key={index} {...element} />;
-        })}
+      <div className="container">
+        <div className="SectorCards">
+          {cardsInfo.map((element, index) => {
+            return <SectorCard key={index} {...element} />;
+          })}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
